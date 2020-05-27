95199514c8bd83c9536b8eef4d32d368e613a92f
alouisrogers <rogers.a@husky.neu.edu>
2020-05-27 14:40:22 -0400

    Added iges

---
+++ venv/Scripts/stl-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\Anthony\PycharmProjects\5AxisSlicer\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'numpy-stl==2.11.2','console_scripts','stl'
+__requires__ = 'numpy-stl==2.11.2'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('numpy-stl==2.11.2', 'console_scripts', 'stl')()
+    )
